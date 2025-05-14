b60e0ee154a2971ba47cfb832abc929d0a6e4bb8
EloM28 <mugisheloge@gmail.com>
2025-05-14 11:29:16 +0200

    Modified the main code

--- gemai/app.py
+++ gemai/app.py
@@ -1,22 +1,24 @@
-import google.generativeai as genai
+def main():
+    try:
+        from .local_settings import API_KEY
+    except ImportError:
+        ...
 
-
-try:
-    from .local_settings import API_KEY
-except ImportError:
-    ...
+    import google.generativeai as genai
     
-print("APIKEY:",API_KEY)
-genai.configure(api_key=API_KEY)
+    genai.configure(api_key=API_KEY)
 
-model = genai.GenerativeModel("gemini-2.0-flash")
-chat = model.start_chat()
+    model = genai.GenerativeModel("gemini-2.0-flash")
+    chat = model.start_chat()
 
-print("Chat with Gemini! Type 'exit' to quit")
+    print("Chat with Gemai! Type 'exit' to quit")
 
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == 'exit':
-        break
-    response = chat.send_message(user_input)
-    print("Gemini: ", response.text)
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            break
+        response = chat.send_message(user_input)
+        print("Gemai: ", response.text)
+
+if __name__ == "__main__":
+    main()
